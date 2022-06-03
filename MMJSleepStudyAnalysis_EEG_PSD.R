95feef1f6acae81bc8a731df7fee6a7dfa98b5bf
ianarriagamackenzie <iansamackenzie@gmail.com>
2022-06-03 10:13:37 -0600

    upload project code
    update example code

---
+++ MMJSleepStudyAnalysis_EEG_PSD.R
@@ -0,0 +1,301 @@
+## MMJ Sleep Study Analysis R file
+## Ian Arriaga Mackenzie
+
+# load libraries
+library(edf)
+library(tidyverse)
+library(RColorBrewer)
+library(eegkit)
+library(reshape2)
+library(signal)
+library(psd)
+library(bspec)
+library(flux)
+library(e1071)
+library(ggplot2)
+library(factoextra)
+library(cluster)
+
+
+# load overall master excel sheet with participant info
+anondatacsv <- read.csv("~/MJsleepstudy/anondatacsv.csv")
+# filter for usable thc use and clock time
+filtdat = anondatacsv %>% 
+  dplyr::filter(THCMinutesSleep != '.') %>% 
+  dplyr::filter(Lightsoffclocktime != '.')
+# filter for no THC use
+nothc = filtdat  %>% 
+  dplyr::filter(THCMinutesSleep == '0')
+# filter for THC use
+yesthc = filtdat %>% 
+  dplyr::filter(THCMinutesSleep != '0') 
+# change to numeric and choose a middle value of THC use
+yesthc$perTHCSleepMODG = as.numeric(yesthc$perTHCSleepMODG)
+yesthcfil = yesthc %>% 
+  dplyr::filter(perTHCSleepMODG < 0.7 & perTHCSleepMODG > 0.2) %>% 
+  dplyr::sample_n(23)
+
+
+# EEG sampling rate
+samprate = 200
+n2int = samprate * 60 * 5
+
+testint = 200 * 10
+# generate values to pull
+teststart = sample(c(1:60000), 6)
+testend = teststart + testint
+print(teststart); print(testend)
+
+# six 10 second interval samples
+teststart = c(34027, 17043, 38171, 47211, 56521, 26842)
+testend = c(36027, 19043, 40171, 49211, 58521, 28842)
+
+# sensor names
+sensorv = c('A1', 'O2', 'C4', 'C3', 'O1')
+# initialize output frame
+ff = data.frame(id = character(),
+                smoke = character(),
+                time = character(),
+                timestart = character(),
+                delta = numeric(),
+                theta = numeric(),
+                alpha1 = numeric(),
+                alpha2 = numeric(),
+                beta = numeric(),
+                gamma = numeric(),
+                tensecinv = numeric(),
+                sensor = character())
+# subject ID
+ins = c('NJT0884')
+# smoke statues
+smoke = 'no'
+# load EDF file
+eegmerge = read.edf(paste('~/MJsleepstudy/usable/', ins,'/', ins, '.edf.edf', 
+                          sep = ''))
+
+# set times for early/late N2 sleep
+tstart = eegmerge[[1]]$timestamp.start; print(tstart)
+tend = eegmerge[[1]]$timestamp.stop; print(tend)
+earlystart = c('2017-03-13 23:35:00 MDT')
+latestart = c('2017-03-14 05:35:00 MDT')
+
+# begin analysis loop
+{
+  # starting sample point
+  tearst = abs(as.numeric(difftime(tstart, earlystart, units = 'secs'))) * 200
+  tearen = tearst + n2int - 1
+  # ending sample point
+  tlatst = abs(as.numeric(difftime(tstart, latestart, units = 'secs'))) * 200
+  tlaten = tlatst + n2int - 1
+  # create list
+  tf_eeg = list()
+  # select EEG leads only, for early N2
+  tf_eeg$early = data.frame(
+    Time = eegmerge$signal$EEG_A1_A2$t[tearst:tearen],
+    EEG_A1_A2 = eegmerge$signal$EEG_A1_A2$data[tearst:tearen],
+    EEG_O2_A1 = eegmerge$signal$EEG_O2_A1$data[tearst:tearen],
+    EEG_C4_A1 = eegmerge$signal$EEG_C4_A1$data[tearst:tearen],
+    EEG_C3_A2 = eegmerge$signal$EEG_C3_A2$data[tearst:tearen],
+    EEG_O1_A2 = eegmerge$signal$EEG_O1_A2$data[tearst:tearen]
+  )
+  # select EEG leads only, for late N2
+  tf_eeg$late = data.frame(
+    Time = eegmerge$signal$EEG_A1_A2$t[tlatst:tlaten],
+    EEG_A1_A2 = eegmerge$signal$EEG_A1_A2$data[tlatst:tlaten],
+    EEG_O2_A1 = eegmerge$signal$EEG_O2_A1$data[tlatst:tlaten],
+    EEG_C4_A1 = eegmerge$signal$EEG_C4_A1$data[tlatst:tlaten],
+    EEG_C3_A2 = eegmerge$signal$EEG_C3_A2$data[tlatst:tlaten],
+    EEG_O1_A2 = eegmerge$signal$EEG_O1_A2$data[tlatst:tlaten]
+  )
+  # loop over early and late
+  for (i in 1:2){
+    
+    tslice = tf_eeg[[i]]
+    
+    earlylatef = c('early', 'late')
+    timestartv = c(earlystart, latestart)
+    # loop over all 6 intervals
+    for (n in 1:6){
+      
+      tslice2 = tslice %>% 
+        slice(teststart[n]:testend[n])
+      # basic bandwidtch filter
+      earlyfilt = eegfilter(tslice2[,2:6], 200, .5, 70, method = 'fir1')
+      
+      # loop over all 5 EEG leads
+      for (j in 1:5){
+        
+        # estimate PSD
+        PSDest = pspectrum(earlyfilt[,j], 200, plot = FALSE, AR = FALSE, Nyquist.normalize = TRUE)
+        
+        psdframe = data.frame(PSDest$freq, 
+                              PSDest$spec)
+        # relative PSD for each bandwidtch
+        psdtot = psdframe %>% 
+          dplyr::filter(PSDest.freq < 40 & PSDest.freq > 0.5)
+        psddelta = psdtot %>% 
+          dplyr::filter(PSDest.freq > 0.5 & PSDest.freq < 4)
+        psdtheta = psdtot %>% 
+          dplyr::filter(PSDest.freq > 4 & PSDest.freq < 7)
+        psdalpha1 = psdtot %>% 
+          dplyr::filter(PSDest.freq > 7 & PSDest.freq < 10)
+        psdalpha2 = psdtot %>% 
+          dplyr::filter(PSDest.freq > 10 & PSDest.freq < 13)
+        psdbeta = psdtot %>% 
+          dplyr::filter(PSDest.freq > 13 & PSDest.freq < 30)
+        psdgamma = psdtot %>% 
+          dplyr::filter(PSDest.freq > 30 & PSDest.freq < 40)
+        # divide by total AUC
+        totalpsd = auc(psdtot$PSDest.freq, psdtot$PSDest.spec)
+        deltar = auc(psddelta$PSDest.freq, psddelta$PSDest.spec)/totalpsd
+        thetar = auc(psdtheta$PSDest.freq, psdtheta$PSDest.spec)/totalpsd
+        alpha1r = auc(psdalpha1$PSDest.freq, psdalpha1$PSDest.spec)/totalpsd
+        alpha2r = auc(psdalpha2$PSDest.freq, psdalpha2$PSDest.spec)/totalpsd
+        betar = auc(psdbeta$PSDest.freq, psdbeta$PSDest.spec)/totalpsd
+        gammar = auc(psdgamma$PSDest.freq, psdgamma$PSDest.spec)/totalpsd
+        # normalize
+        wavefrac = c(deltar, thetar, alpha1r, alpha2r, betar, gammar)/sum(deltar, thetar, alpha1r, alpha2r, betar, gammar)
+        # save to output frame
+        outline = data.frame(id = ins,
+                             smoke = smoke,
+                             time = earlylatef[i],
+                             timestart = timestartv[i],
+                             delta = wavefrac[1],
+                             theta = wavefrac[2],
+                             alpha1 = wavefrac[3],
+                             alpha2 = wavefrac[4],
+                             beta = wavefrac[5],
+                             gamma = wavefrac[6],
+                             tensecinv = n,
+                             sensor = sensorv[j])
+        # bind final output
+        ff = rbind(ff, outline)
+      }
+    }
+  }
+}
+
+# write out table
+write.table(ff, file = "PSD_mmjstudy.txt")
+# read in out table
+PSD_mmjstudy <- read.csv("~/sleepfiles/PSD_mmjstudy.txt", sep="")
+
+# save to seperate DF
+ff = PSD_mmjstudy
+
+# average leads by subject
+avepsd = ff[,5:10]
+n = 5
+avepsd2 = aggregate(avepsd,list(rep(1:(nrow(avepsd)%/%n+1),each=n,len=nrow(avepsd))),mean)[-1];
+extractrow = ff[seq(1,2160, by = n), c(1:4,11)]
+ff_av = cbind(extractrow, avepsd2)
+ff_av2 = cbind(extractrow, avepsd2) %>% 
+  dplyr::filter(time == 'late')
+
+# early and late plots
+plotint = ff_av
+plotint2 = ff_av2
+# plot labels
+smoke.labs <- c("No THC", "Yes THC")
+names(smoke.labs) <- c("no", "yes")
+time.labs <- c("Early N2 Sleep", "Late N2 Sleep")
+names(time.labs) <- c("early", "late")
+
+# testing plot
+ggplot(data = plotint, aes(y = gamma)) +
+  geom_boxplot() + 
+  labs(y = 'Relative PSD', title = 'Gamma') +
+  facet_grid(time ~ smoke, 
+             labeller = labeller(time = time.labs, smoke = smoke.labs))
+
+# boxplots and two way ANOVA
+{boxplot(delta ~ time + smoke, data = plotint)
+  res.aov = aov(delta ~ time + smoke, data = plotint)
+  summary(res.aov)}
+{boxplot(theta ~ time + smoke, data = plotint)
+  res.aov = aov(theta ~ time + smoke, data = plotint)
+  summary(res.aov)}
+{boxplot(alpha1 ~ time + smoke, data = plotint)
+  res.aov = aov(alpha1 ~ time + smoke, data = plotint)
+  summary(res.aov)}
+{boxplot(alpha2 ~ time + smoke, data = plotint)
+  res.aov = aov(alpha2 ~ time + smoke, data = plotint)
+  summary(res.aov)}
+{boxplot(beta ~ time + smoke, data = plotint)
+  res.aov = aov(beta ~ time + smoke, data = plotint)
+  summary(res.aov)}
+{boxplot(gamma ~ time + smoke, data = plotint)
+  res.aov = aov(gamma ~ time + smoke, data = plotint)
+  summary(res.aov)}
+
+# boxplots and one way ANOVA
+{boxplot(delta ~ smoke, data = plotint2)
+  res.aov = aov(delta ~ smoke, data = plotint2)
+  summary(res.aov)}
+{boxplot(theta ~ smoke, data = plotint2)
+  res.aov = aov(theta ~ smoke, data = plotint2)
+  summary(res.aov)}
+{boxplot(alpha1 ~ smoke, data = plotint2)
+  res.aov = aov(alpha1 ~ smoke, data = plotint2)
+  summary(res.aov)}
+{boxplot(alpha2 ~ smoke, data = plotint2)
+  res.aov = aov(alpha2 ~ smoke, data = plotint2)
+  summary(res.aov)}
+{boxplot(beta ~ smoke, data = plotint2)
+  res.aov = aov(beta ~ smoke, data = plotint)
+  summary(res.aov)}
+{boxplot(gamma ~ smoke, data = plotint2)
+  res.aov = aov(gamma ~ smoke, data = plotint2)
+  summary(res.aov)}
+
+
+
+# PCA and K-means Testing, NOT USED IN REPORT
+# Slight interesting groupings when principle component analysis done, may be useful moving forward
+clusterdf = ff_av[,6:11]
+n = 6
+clusterdf2 = aggregate(clusterdf,list(rep(1:(nrow(clusterdf)%/%n+1),each=n,len=nrow(clusterdf))),mean)[-1]
+
+namestest = ff_av$id[seq(1, 432, by = 12)]
+namestest1 = paste0(namestest[1:18], rep('_early_smoke', 18))
+namestest2 = paste0(namestest[1:18], rep('_late_smoke', 18))
+namestest3 = paste0(namestest[19:36], rep('_early_nosmoke', 18))
+namestest4 = paste0(namestest[19:36], rep('_late_nosmoke', 18))
+
+namestest5 = c(rbind(namestest1, namestest2))
+namestest6 = c(rbind(namestest3, namestest4))
+namestest7 = c(namestest5, namestest6)
+
+groups = c(c(rbind(rep('early_smoke', 18), rep('late_smoke', 18))), c(rbind(rep('early_nosmoke', 18), rep('late_nosmoke', 18))))
+
+row.names(clusterdf2) = namestest7
+
+k2 <- kmeans(clusterdf2, centers = 4, nstart = 25)
+fviz_cluster(k2, data = clusterdf2)
+
+res.pca <- prcomp(clusterdf2, scale. = TRUE)
+
+fviz_pca_ind(res.pca,
+             col.ind = "cos2",
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07")
+)
+
+fviz_pca_var(res.pca,
+             col.var = "contrib",
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE
+)
+
+
+groups = as.factor(c(c(rbind(rep('Early THC', 18), rep('Late THC', 18))), c(rbind(rep('Early NoTHC', 18), rep('Late NoTHC', 18)))))
+
+fviz_pca_ind(res.pca,
+             habillage = groups,
+             geom = 'point',
+             addEllipses = TRUE,
+             ellipse.type = "confidence",
+             legend.title = "Groups"
+)
+
+distance <- get_dist(clusterdf2)
+fviz_dist(distance, gradient = list(low = "#00AFBB", mid = "white", high = "#FC4E07"))
---
+++ MethodsReport_Rexample_IanArriagaMacKenzie.R
@@ -0,0 +1,189 @@
+# Method Paper - PSD
+# Ian Arriaga MacKenzie
+
+library(tidyverse)
+library(reshape2)
+library(eegkit)
+library(psd)
+library(RColorBrewer)
+library(edf)
+
+
+# Simulate data, similar to sampled EEG sleep data
+
+# Sampling Rate (Hz)
+n = 200
+# 10 seconds of data
+s = 10
+# time vector at 200 Hz
+t = seq(0, s, length.out = s*n)
+
+# sinusoidal wave with signal at 1 Hz
+s1 = sin(2*pi*t*1)
+# signal at 5 Hz
+s2 = sin(2*pi*t*5)
+# signal at 10 Hz
+s3 = sin(2*pi*t*10)
+# signal at 20 Hz
+s4 = sin(2*pi*t*20)
+# signal at 45 Hz
+s5 = sin(2*pi*t*45)
+
+# combine all signals into one
+comb_sig = s1 + s2 + s3 + s4 + s5
+
+# generate a gaussian error and add it to the combined signal
+g_err = rnorm(s * n, sd = 1)
+comb_sig_err = comb_sig + g_err
+
+# Apply a 3 Hz - 30 Hz band-pass FIR filter to the signal with error
+fir_filt = eegfilter(comb_sig_err, Fs = n, lower = 3, upper = 30, method = "fir1", order = 300)
+
+# combine all signals into a data frame for plotting
+signalframe = data.frame(
+  time = t,
+  signal1 = s1,
+  signal2 = s2,
+  signal3 = s3,
+  signal4 = s4,
+  signal5 = s5,
+  combsig = comb_sig,
+  combsig_err = comb_sig_err,
+  combsig_FIR = fir_filt
+)
+
+# select individual signals
+signalframe_solo = signalframe %>% 
+  select(time, signal1, signal2, signal2, signal3, signal4, signal5) %>% 
+  rename("Signal 1" = signal1, "Signal 2" = signal2, "Signal 3" = signal3, "Signal 4" = signal4, "Signal 5" = signal5) %>% 
+  melt(id.vars = "time")
+# plot individual signals
+ggplot(signalframe_solo, aes(time, value)) +
+  geom_line() +
+  facet_grid(variable ~ .) +
+  labs(x = "Time", y = NULL) +
+  theme_bw()
+
+# select combined signals, error, and band pass
+combframe = signalframe %>% 
+  select(time, combsig, combsig_err, combsig_FIR) %>% 
+  melt(id.vars = "time")
+levels(combframe$variable) = c("Combined Signal", "Comb. Signal + Error", "Comb. Signal + FIR Filter")
+# plot combined signals
+ggplot(combframe, aes(time, value)) +
+  geom_line() +
+  facet_grid(variable ~ .) +
+  theme_bw()
+
+
+# Power Spectral Density - shows signals present in combined data
+# decibals
+# PSD of combined signal
+eegpsd(signalframe$combsig_err, Fs = n, upper = 50, t = "b")
+# PSD of band-pass signal
+eegpsd(signalframe$combsig_FIR, Fs = n, upper = 50, t = "b")
+
+# squared microvolts
+# PSD of combined signal
+eegpsd(signalframe$combsig_err, Fs = n, upper = 50, unit = "mV^2", t = "b")
+# PSD of band-pass signal
+eegpsd(signalframe$combsig_FIR, Fs = n, upper = 50, unit = "mV^2", t = "b")
+
+
+
+# Use of multitaper power spectral density estimation
+
+# PSD of combined signal
+PSDest = pspectrum(signalframe$combsig_err, 200)
+# Extract values for plotting
+psdframe = data.frame(freq = PSDest$freq, 
+                      spec = PSDest$spec)
+# Trim data
+psdframe_trim = psdframe[1:500,]
+# Plot, vertical lines at simulated signal
+linecol = brewer.pal(6, "Paired")[6]
+ggplot(psdframe_trim, aes(freq, spec)) +
+  geom_vline(xintercept = 1, color = linecol, size = 1)+
+  geom_vline(xintercept = 5, color = linecol, size = 1)+
+  geom_vline(xintercept = 10, color = linecol, size = 1)+
+  geom_vline(xintercept = 20, color = linecol, size = 1)+
+  geom_vline(xintercept = 45, color = linecol, size = 1) +
+  geom_point(size = 2, alpha = 0.6) +
+  labs(x = "Frequency", y = "Power") +
+  theme_bw()
+
+
+# PSD of band-pass signal
+PSDest_fir = pspectrum(signalframe$combsig_FIR, 200)
+# Extract values for plotting
+psdframe_fir = data.frame(freq = PSDest_fir$freq, 
+                      spec = PSDest_fir$spec)
+# Trim data
+psdframe_trim_fir = psdframe_fir[1:500,]
+# Plot, vertical lines at simulated signal
+ggplot(psdframe_trim_fir, aes(freq, spec)) +
+  geom_vline(xintercept = 5, color = linecol, size = 1)+
+  geom_vline(xintercept = 10, color = linecol, size = 1)+
+  geom_vline(xintercept = 20, color = linecol, size = 1) +
+  geom_point(size = 2, alpha = 0.5) +
+  labs(x = "Frequency", y = "Power") +
+  theme_bw()
+
+
+
+
+
+
+# EEG sampling rate
+samprate = 200
+
+# load EDF file
+eegmerge = read.edf(paste('~/NJT0884.edf.edf', 
+                          sep = ''))
+
+# early time
+tstart = eegmerge[[1]]$timestamp.start
+earlystart = c('2017-03-13 23:35:00 MDT')
+
+
+# starting sample point
+tearst = abs(as.numeric(difftime(tstart, earlystart, units = 'secs'))) * 200
+tearen = tearst + 199
+
+# select EEG lead, also FIR filter EEG lead
+tf_eeg = data.frame(
+  Time = 0:1999,
+  EEG_A1_A2 = eegmerge$signal$EEG_A1_A2$data[tearst:tearen],
+  FIR_A1_A2 = eegfilter(eegmerge$signal$EEG_A1_A2$data[tearst:tearen], Fs = 200, lower = 0.5, upper = 40, method = "fir1", order = 300)
+)
+
+# Plot raw/filtered EEG lead
+eeg_plot = tf_eeg %>% 
+  melt(id.vars = "Time")
+levels(eeg_plot$variable) = c("A1 - A2", "A1 - A2, FIR Filter")
+ggplot(eeg_plot, aes(Time, value)) +
+  geom_line() +
+  facet_grid(variable ~ .) +
+  theme_bw() +
+  labs(x = "Time (200 Hz Sampling Rate)", y = NULL)
+
+# PSD estimation on raw/filtered signal
+tf_psd = data.frame(
+  PSD = pspectrum(tf_eeg$EEG_A1_A2, 200),
+  PSD_FIR = pspectrum(tf_eeg$FIR_A1_A2, 200)
+)
+
+# Plot the PSD results, for raw and filtered signal
+ggplot(tf_psd, aes(PSD.freq, PSD.spec)) +
+  geom_line(size = 1) +
+  xlim(0, 40) +
+  labs(x = "Frequency", y = "Spectrum") +
+  theme_bw()
+
+ggplot(tf_psd, aes(PSD_FIR.freq, PSD_FIR.spec)) +
+  geom_line(size = 1) +
+  xlim(0, 40) +
+  labs(x = "Frequency", y = "Spectrum") +
+  theme_bw()
+
+
